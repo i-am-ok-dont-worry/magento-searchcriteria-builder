44b75374b43fe584201a652627774947553d0f7b
Mateusz <mateusz.pytel@grupakmk.pl>
2021-01-06 19:01:42 +0100

    SearchCriteria class added

---
+++ index.js
@@ -0,0 +1,45 @@
+import qs from 'qs';
+
+class SearchCriteria {
+    filterGroups = [];
+    pageSize = 50;
+    currentPage = 0;
+    sortOrders = [];
+
+    applyFilter (field, value, conditionType) {
+        if (!field) { return; }
+        const filter = { field, value, conditionType: conditionType || 'eq' };
+        const filters = [filter];
+
+        this.filterGroups.push({ filters });
+    }
+
+    setPageSize (pageSize) {
+        this.pageSize = pageSize || 50;
+    }
+
+    setCurrentPage (currentPage) {
+        this.currentPage = currentPage || 0;
+    }
+
+    applySort (field, direction) {
+        if (!field) { return; }
+        const sortOrders = { field, direction: direction || 'asc' };
+        this.sortOrders.push(sortOrders);
+    }
+
+    build () {
+        const output = {
+            searchCriteria: {
+                filterGroups: this.filterGroups,
+                ...(this.sortOrders.length && { sortOrders: this.sortOrders }),
+                pageSize: this.pageSize,
+                currentPage: this.currentPage
+            }
+        };
+
+        return qs.stringify(output, { arrayFormat: 'bracket' });
+    }
+}
+
+module.exports = SearchCriteria;
